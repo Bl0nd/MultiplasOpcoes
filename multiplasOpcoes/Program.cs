f9fbe318950b57e732e4623b0786b585d727c4c2
FERNANDA ARAUJO <fernanda.adesanto@senacsp.edu.br>
2024-04-29 21:43:41 -0300

    while

--- multiplasOpcoes/Program.cs
+++ multiplasOpcoes/Program.cs
@@ -9,29 +9,42 @@ num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Digite outro número: ");
 num2 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Digite uma opção abaixo: ");
-Console.WriteLine("1 - Somar");
-Console.WriteLine("2 - Subtrair");
-Console.WriteLine("3 - Multiplicar");
-Console.WriteLine("4 - Dividir");
+//faça enquanto
 
-opcao = Convert.ToInt32(Console.ReadLine());
-switch (opcao)
+do
 {
-    case 1:
-        resultado = num1 + num2;
-        break;
-    case 2:
-        resultado = num1 - num2;
-        break;
-    case 3:
-        resultado = num1 * num2;
-        break;
-    case 4:
-        resultado = num1 / num2;
-        break;
-    default:
-        Console.WriteLine("Opção inválida");
-        break;
+    Console.WriteLine("Digite uma opção abaixo: ");
+    Console.WriteLine("1 - Somar");
+    Console.WriteLine("2 - Subtrair");
+    Console.WriteLine("3 - Multiplicar");
+    Console.WriteLine("4 - Dividir");
+    Console.WriteLine("9 - Finalizar");
+
+    opcao = Convert.ToInt32(Console.ReadLine());
+    switch (opcao)
+    {
+        case 1:
+            resultado = num1 + num2;
+            break;
+        case 2:
+            resultado = num1 - num2;
+            break;
+        case 3:
+            resultado = num1 * num2;
+            break;
+        case 4:
+            resultado = num1 / num2;
+            break;
+        case 9:
+            break;
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
+    }
+
+    if (opcao != 9)
+    {
+        Console.WriteLine("Resultado: " + resultado);
+    }
 }
-Console.WriteLine("Resultado: " + resultado);
+while (opcao != 9);
